0cd974952b67dbc937492a34b40f399159cbc63e
LSayers-Designs <58526383+LSayers-Designs@users.noreply.github.com>
2020-05-04 18:01:37 -0400

    updates 5.4.2020

--- gottp.py
+++ gottp.py
@@ -7,19 +7,20 @@ Last Revised: 5/3/2020
 Description: Calculates toilet paper stash per household to determine if you are a hoarder
 """
 
-from breezypythongui import EasyFrame
+from breezypythongui import EasyFrame 
+import math
 
 class gottp(EasyFrame):
 	"""GUI style framework"""
 	def __init__(self):
-		EasyFrame.__init__(self, title = "2-Ply Toilet Paper Calculator for a Household", background = "lightblue",width = 800, height = 400)
+		EasyFrame.__init__(self, title = "2-Ply Toilet Paper Calculator for a Household", background = "cadetblue",width = 800, height = 400)
 		#Labels for the window 
-		self.addLabel(text = "Got Enough Toilet Paper?", row = 0, column = 0, columnspan = 100, sticky = "NSEW", background = "white", foreground = "darkgreen", font="bold")
-		self.addLabel(text = "Number of adult women:", row = 1, column = 0, columnspan = 10, background = "lightblue", foreground = "white", font="bold")
-		self.addLabel(text = "Number of adult men:", row = 2, column = 0, columnspan = 10, background = "lightblue", foreground = "white", font="bold")
-		self.addLabel(text = "Number of children (11 years and under):", row = 3, column = 0, columnspan = 10, background = "lightblue", foreground = "white", font="bold")
-		self.addLabel(text = "How many rolls of toilet paper do you currently have in stock?", row = 4, column = 0, columnspan = 10, background = "lightblue", foreground = "white", font="bold")
-		self.addLabel(text = "How many sheets per roll in stock (see the packaging for details or otherwise guess)", row = 5, column = 0, columnspan = 10, background = "lightblue", foreground = "white", font="bold")
+		self.addLabel(text = "Got Enough Toilet Paper?", row = 0, column = 0, columnspan = 100, sticky = "NSEW", background = "white", foreground = "darkcyan", font="bold")
+		self.addLabel(text = "Number of adult women:", row = 1, column = 0, columnspan = 10, background = "cadetblue", foreground = "white", font="bold")
+		self.addLabel(text = "Number of adult men:", row = 2, column = 0, columnspan = 10, background = "cadetblue", foreground = "white", font="bold")
+		self.addLabel(text = "Number of children (11 years and under):", row = 3, column = 0, columnspan = 10, background = "cadetblue", foreground = "white", font="bold")
+		self.addLabel(text = "How many rolls of toilet paper do you currently have in stock?", row = 4, column = 0, columnspan = 10, background = "cadetblue", foreground = "white", font="bold")
+		self.addLabel(text = "How many sheets per roll in stock (see the packaging for details or otherwise guess)", row = 5, column = 0, columnspan = 10, background = "cadetblue", foreground = "white", font="bold")
 
 		#and input area for data collection
 		self.women = self.addIntegerField(value = 0, row = 1, column = 1, width = 10)
@@ -35,13 +36,9 @@ class gottp(EasyFrame):
 	#Event handling and calculations
 	def calculate(self):	
 		#Modus Tollens
-		p = 21
-		q = 9
-		r = 9
-
-		j = 7
-		k = 7
-		l = 8
+		p = 89
+		q = 62
+		r = 44
 
 		#Get inputs
 		female = self.women.getNumber()
@@ -52,9 +49,9 @@ class gottp(EasyFrame):
 
 		#Compute Inputs
 		roll = str(sheetspr)
-		rollph = str(sheetspr/((p*j*female)+(q*k*male)+(r*l*kids)))
-		rstock = str((sheetspr/((p*j*female)+(q*k*male)+(r*l*kids))) * onhand)
-		facts = ((sheetspr/((p*j*female)+(q*k*male)+(r*l*kids))) * onhand)
+		rollph = str((sheetspr*2)/((p*female)+(q*male)+(r*kids)))
+		rstock = str(((sheetspr*2)/((p*female)+(q*male)+(r*kids))) * onhand)
+		facts = (((sheetspr*2)/((p*female)+(q*male)+(r*kids))) * onhand)
 		
 
 		#Additional Comment Statments
@@ -69,8 +66,13 @@ class gottp(EasyFrame):
 		else:
 			comment = ("You should feel ashamed of yourself, you hoarder!!")
 
+		if rollph == 1:
+			days = ("day")	
+		else:
+			days = ("days")
+
 		#Output Statements
-		conclusion = ("A single " + roll + " sheet 2-ply roll of toilet paper will last your household " + rollph + " days. \nYou currently have a " + rstock + " day supply. \n" + comment)
+		conclusion = ("A single " + roll + " sheet 2-ply roll of toilet paper will last your household " + rollph +" "+ days + ". \nYou currently have a " + rstock + " day supply. \n" + comment)
 		
 		
 		#Append Results 
